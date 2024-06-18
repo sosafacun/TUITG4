14e18e3c347631c25066a77634ed0bb7547e020e
sosafacun <facundososa+github@slmail.me>
2024-06-18 17:52:54 -0300

    Creating Enemies
    8:46:00

--- Shooter/scenes/levels/inside.gd
+++ Shooter/scenes/levels/inside.gd
@@ -3,4 +3,4 @@ extends LevelParent
 func _on_exit_gate_area_body_entered(_body):
 	var set_player_speed_tween = get_tree().create_tween()
 	set_player_speed_tween.tween_property($Player, "speed", 0, 0.5)
-	get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
+	TransitionLayer.change_scene("res://scenes/levels/outside.tscn")
