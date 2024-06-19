aa8187b9d19d5526e8c65fcab24eb5e1384e049d
sosafacun <facundososa+github@slmail.me>
2024-06-19 16:51:49 -0300

    Creating the Grenade
    Exercise done

--- Shooter/scenes/projectiles/grenade.gd
+++ Shooter/scenes/projectiles/grenade.gd
@@ -4,3 +4,8 @@ const speed = 750
 
 func explode():
 	$Explosion/GrenadeExplosionAnimation.play("explosion_animation")
+
+
+func _on_effective_blast_radius_body_entered(body):
+	if (body != $"."):
+		print('grenade hit')
